9f4a7ec8f7bf60366b6942461d536b1175b2f9bb
john-kelly <johnkellytemena@gmail.com>
2025-03-27 12:04:45 +0800

    Add edit task function

--- src/modules/task-manager.js
+++ src/modules/task-manager.js
@@ -32,6 +32,26 @@ export default class TaskManager {
         this.#projectList[projectId].addTask(task, this.#taskIdCounter++);
     }
 
+    editTask(projectId, taskId, title, description, dueDate, priority) {
+        let task = this.#projectList[projectId].getTask(taskId);
+
+        if (title !== null) {
+            task.title = title;
+        }
+
+        if (description !== null) {
+            task.description = description;
+        }
+
+        if (dueDate !== null) {
+            task.dueDate = dueDate;
+        }
+
+        if (priority !== null) {
+            task.priority = priority;
+        }
+    }
+
     newProject(projectTitle) {
         let project = new Project(projectTitle);
         this.#projectList[this.#projectIdCounter++] = project;
