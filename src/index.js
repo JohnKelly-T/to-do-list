fc3847ead20c11d5fa0a75ae36395a29e0d17768
john-kelly <johnkellytemena@gmail.com>
2025-03-31 16:38:03 +0800

    Import TaskManager Class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles/reset.css";
 import "./styles/global.css";
 import displayDate from "./modules/display-date";
+import TaskManager from task-manager.js;
 
 displayDate();
+
+let taskManager = new TaskManager();
