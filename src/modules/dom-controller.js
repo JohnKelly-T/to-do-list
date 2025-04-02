54312e1333262e7cdd1866e389833aea8cf40024
john-kelly <johnkellytemena@gmail.com>
2025-04-02 21:22:36 +0800

    Refactor loadTasksPage to loadProjectPage
    loadTasksPage was originally meant to be a general task page loader but
    at this point the other pages would be too different from the page
    with different card types so they would be created separately

--- src/modules/dom-controller.js
+++ src/modules/dom-controller.js
@@ -15,9 +15,7 @@ export default class DOMController {
             
             if (navButton) {
                 let projectId = Number(navButton.dataset.id);
-                let projectTitle = this.taskManager.getProject(projectId).title;
-                let taskList = this.taskManager.getProjectTasks(projectId);
-                this.loadTasksPage(projectTitle, taskList);
+                this.loadProjectPage(projectId);
             }
         });
 
@@ -57,7 +55,7 @@ export default class DOMController {
         });
     }
 
-    loadTasksPage(headerTitle, taskList, withAddButton = true) {
+    loadProjectPage(projectId) {
         let container = document.querySelector(".content-view-container");
 
         let pageHeader = document.createElement("div");
@@ -65,14 +63,14 @@ export default class DOMController {
 
         let pageTitle = document.createElement("div");
         pageTitle.classList.add("page-title");
-        pageTitle.textContent = headerTitle;
+        pageTitle.textContent = this.taskManager.getProject(projectId).title;
 
         let addTaskButton = document.createElement("button");
 
-        if (withAddButton) {
-            addTaskButton.classList.add("add-task-button"); 
-            addTaskButton.innerHTML = 'Add Task <svg class="add-task-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M11.5 12.5V16q0 .213.144.356t.357.144t.356-.144T12.5 16v-3.5H16q.213 0 .356-.144t.144-.357t-.144-.356T16 11.5h-3.5V8q0-.213-.144-.356t-.357-.144t-.356.144T11.5 8v3.5H8q-.213 0-.356.144t-.144.357t.144.356T8 12.5zm.503 8.5q-1.867 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709"/></svg>';    
-        }
+
+        addTaskButton.classList.add("add-task-button"); 
+        addTaskButton.innerHTML = 'Add Task <svg class="add-task-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M11.5 12.5V16q0 .213.144.356t.357.144t.356-.144T12.5 16v-3.5H16q.213 0 .356-.144t.144-.357t-.144-.356T16 11.5h-3.5V8q0-.213-.144-.356t-.357-.144t-.356.144T11.5 8v3.5H8q-.213 0-.356.144t-.144.357t.144.356T8 12.5zm.503 8.5q-1.867 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709"/></svg>';    
+
         
         let sortDropdown = document.createElement("div");
         sortDropdown.classList.add("sort-dropdown");
@@ -104,13 +102,10 @@ export default class DOMController {
         sortDropdown.appendChild(sortDropdownMenu);
 
         pageHeader.appendChild(pageTitle);
-
-        if (withAddButton) pageHeader.appendChild(addTaskButton);
-
+        pageHeader.appendChild(addTaskButton);
         pageHeader.appendChild(sortDropdown);
 
         container.appendChild(pageHeader);
-
     }
 
     loadProjects() {
