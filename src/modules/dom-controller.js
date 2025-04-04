22ecd345a89c5c1db0a1194efc968567d349e38b
john-kelly <johnkellytemena@gmail.com>
2025-04-04 18:58:24 +0800

    Remove sort buttons

--- src/modules/dom-controller.js
+++ src/modules/dom-controller.js
@@ -156,39 +156,8 @@ export default class DOMController {
         addTaskButton.classList.add("add-task-button"); 
         addTaskButton.innerHTML = 'Add Task <svg class="add-task-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M11.5 12.5V16q0 .213.144.356t.357.144t.356-.144T12.5 16v-3.5H16q.213 0 .356-.144t.144-.357t-.144-.356T16 11.5h-3.5V8q0-.213-.144-.356t-.357-.144t-.356.144T11.5 8v3.5H8q-.213 0-.356.144t-.144.357t.144.356T8 12.5zm.503 8.5q-1.867 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709"/></svg>';    
 
-        
-        let sortDropdown = document.createElement("div");
-        sortDropdown.classList.add("sort-dropdown");
-
-        let sortButton = document.createElement("button");
-        sortButton.classList.add("sort-button");
-        sortButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m10.5 12.5l4 4.107l4-4.107m-8-4l-4-4l-4 3.997m4-3.997v12m8-12v12" stroke-width="1.2"/></svg>';
-
-        let sortDropdownMenu = document.createElement("div");
-        sortDropdownMenu.classList.add("sort-dropdown-menu");
-
-        let sortDateAdded = document.createElement("button");
-        sortDateAdded.setAttribute("data-value", "date-added");
-        sortDateAdded.textContent = "Date Added";
-
-        let sortPriority = document.createElement("button");
-        sortPriority.setAttribute("data-value", "priority");
-        sortPriority.textContent = "Priority";
-
-        let sortDueDate = document.createElement("button");
-        sortDueDate.setAttribute("data-value", "due-date");
-        sortDueDate.textContent = "Due Date";
-
-        sortDropdownMenu.appendChild(sortDateAdded);
-        sortDropdownMenu.appendChild(sortPriority);
-        sortDropdownMenu.appendChild(sortDueDate);
-
-        sortDropdown.appendChild(sortButton);
-        sortDropdown.appendChild(sortDropdownMenu);
-
         pageHeader.appendChild(pageTitle);
         pageHeader.appendChild(addTaskButton);
-        pageHeader.appendChild(sortDropdown);
 
         container.appendChild(pageHeader);
         
@@ -239,39 +208,8 @@ export default class DOMController {
         addTaskButton.classList.add("add-task-button"); 
         addTaskButton.innerHTML = 'Add Task <svg class="add-task-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M11.5 12.5V16q0 .213.144.356t.357.144t.356-.144T12.5 16v-3.5H16q.213 0 .356-.144t.144-.357t-.144-.356T16 11.5h-3.5V8q0-.213-.144-.356t-.357-.144t-.356.144T11.5 8v3.5H8q-.213 0-.356.144t-.144.357t.144.356T8 12.5zm.503 8.5q-1.867 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709"/></svg>';    
 
-        
-        let sortDropdown = document.createElement("div");
-        sortDropdown.classList.add("sort-dropdown");
-
-        let sortButton = document.createElement("button");
-        sortButton.classList.add("sort-button");
-        sortButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m10.5 12.5l4 4.107l4-4.107m-8-4l-4-4l-4 3.997m4-3.997v12m8-12v12" stroke-width="1.2"/></svg>';
-
-        let sortDropdownMenu = document.createElement("div");
-        sortDropdownMenu.classList.add("sort-dropdown-menu");
-
-        let sortDateAdded = document.createElement("button");
-        sortDateAdded.setAttribute("data-value", "date-added");
-        sortDateAdded.textContent = "Date Added";
-
-        let sortPriority = document.createElement("button");
-        sortPriority.setAttribute("data-value", "priority");
-        sortPriority.textContent = "Priority";
-
-        let sortDueDate = document.createElement("button");
-        sortDueDate.setAttribute("data-value", "due-date");
-        sortDueDate.textContent = "Due Date";
-
-        sortDropdownMenu.appendChild(sortDateAdded);
-        sortDropdownMenu.appendChild(sortPriority);
-        sortDropdownMenu.appendChild(sortDueDate);
-
-        sortDropdown.appendChild(sortButton);
-        sortDropdown.appendChild(sortDropdownMenu);
-
         pageHeader.appendChild(pageTitle);
         pageHeader.appendChild(addTaskButton);
-        pageHeader.appendChild(sortDropdown);
 
         container.appendChild(pageHeader);
         
@@ -360,38 +298,8 @@ export default class DOMController {
         let pageTitle = document.createElement("div");
         pageTitle.classList.add("page-title");
         pageTitle.textContent = "Overdue ❗";
-        
-        let sortDropdown = document.createElement("div");
-        sortDropdown.classList.add("sort-dropdown");
-
-        let sortButton = document.createElement("button");
-        sortButton.classList.add("sort-button");
-        sortButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m10.5 12.5l4 4.107l4-4.107m-8-4l-4-4l-4 3.997m4-3.997v12m8-12v12" stroke-width="1.2"/></svg>';
-
-        let sortDropdownMenu = document.createElement("div");
-        sortDropdownMenu.classList.add("sort-dropdown-menu");
-
-        let sortDateAdded = document.createElement("button");
-        sortDateAdded.setAttribute("data-value", "date-added");
-        sortDateAdded.textContent = "Date Added";
-
-        let sortPriority = document.createElement("button");
-        sortPriority.setAttribute("data-value", "priority");
-        sortPriority.textContent = "Priority";
-
-        let sortDueDate = document.createElement("button");
-        sortDueDate.setAttribute("data-value", "due-date");
-        sortDueDate.textContent = "Due Date";
-
-        sortDropdownMenu.appendChild(sortDateAdded);
-        sortDropdownMenu.appendChild(sortPriority);
-        sortDropdownMenu.appendChild(sortDueDate);
-
-        sortDropdown.appendChild(sortButton);
-        sortDropdown.appendChild(sortDropdownMenu);
 
         pageHeader.appendChild(pageTitle);
-        pageHeader.appendChild(sortDropdown);
 
         container.appendChild(pageHeader);
         
@@ -442,39 +350,8 @@ export default class DOMController {
         addTaskButton.classList.add("add-task-button"); 
         addTaskButton.innerHTML = 'Add Task <svg class="add-task-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M11.5 12.5V16q0 .213.144.356t.357.144t.356-.144T12.5 16v-3.5H16q.213 0 .356-.144t.144-.357t-.144-.356T16 11.5h-3.5V8q0-.213-.144-.356t-.357-.144t-.356.144T11.5 8v3.5H8q-.213 0-.356.144t-.144.357t.144.356T8 12.5zm.503 8.5q-1.867 0-3.51-.708q-1.643-.709-2.859-1.924t-1.925-2.856T3 12.003t.709-3.51Q4.417 6.85 5.63 5.634t2.857-1.925T11.997 3t3.51.709q1.643.708 2.859 1.922t1.925 2.857t.709 3.509t-.708 3.51t-1.924 2.859t-2.856 1.925t-3.509.709"/></svg>';    
 
-        
-        let sortDropdown = document.createElement("div");
-        sortDropdown.classList.add("sort-dropdown");
-
-        let sortButton = document.createElement("button");
-        sortButton.classList.add("sort-button");
-        sortButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m10.5 12.5l4 4.107l4-4.107m-8-4l-4-4l-4 3.997m4-3.997v12m8-12v12" stroke-width="1.2"/></svg>';
-
-        let sortDropdownMenu = document.createElement("div");
-        sortDropdownMenu.classList.add("sort-dropdown-menu");
-
-        let sortDateAdded = document.createElement("button");
-        sortDateAdded.setAttribute("data-value", "date-added");
-        sortDateAdded.textContent = "Date Added";
-
-        let sortPriority = document.createElement("button");
-        sortPriority.setAttribute("data-value", "priority");
-        sortPriority.textContent = "Priority";
-
-        let sortDueDate = document.createElement("button");
-        sortDueDate.setAttribute("data-value", "due-date");
-        sortDueDate.textContent = "Due Date";
-
-        sortDropdownMenu.appendChild(sortDateAdded);
-        sortDropdownMenu.appendChild(sortPriority);
-        sortDropdownMenu.appendChild(sortDueDate);
-
-        sortDropdown.appendChild(sortButton);
-        sortDropdown.appendChild(sortDropdownMenu);
-
         pageHeader.appendChild(pageTitle);
         pageHeader.appendChild(addTaskButton);
-        pageHeader.appendChild(sortDropdown);
 
         container.appendChild(pageHeader);
         
