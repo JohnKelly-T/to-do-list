2e3556b7dc9254b7e5cbe8ed289686036fa1b381
john-kelly <johnkellytemena@gmail.com>
2025-04-05 20:54:30 +0800

    Add localstorage save and load

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import displayDate from "./modules/display-date";
 import TaskManager from "./modules/task-manager.js";
 import DomController from "./modules/dom-controller.js";
 import {add, sub} from "date-fns";
+import saveTasks from "./modules/saveTasks.js";
+import loadTasks from "./modules/loadTasks.js";
+
+displayDate();
 
 // load theme from localStorage
 let storedTheme = localStorage.getItem("theme");
@@ -17,27 +21,30 @@ if (storedTheme) {
     document.documentElement.classList.add(prefersDark ? "dark" : "light");
 }
 
-displayDate();
+// load task manager from local storage
+let taskManager = loadTasks();
 
-let taskManager = new TaskManager();
+// load sample data if save file does not exist
+if (!taskManager) {
+    taskManager = new TaskManager();
+    // create default home project
+    taskManager.newProject("Home 🏠");
 
-// create default home project
-taskManager.newProject("Home 🏠");
+    // create quick task project
+    taskManager.newProject("Quick Tasks");
 
-// create quick task project
-taskManager.newProject("Quick Tasks");
+    // create sample tasks
 
-// create sample 
-// normal task no date no priority
-taskManager.newTask(0, "Completed Task", "This is a sample task", null, null);
-taskManager.completeTask(0, 0);
+    taskManager.newTask(0, "Completed Task", "This is a sample task", null, null);
+    taskManager.completeTask(0, 0);
+    taskManager.newTask(0, "High Priority task today", "This is a sample task",  new Date(Date.now()), "high");
+    taskManager.newTask(0, "Medium Priority task upcoming", "This is a sample task", new add(Date.now(), {"years": 1}), "medium");
+    taskManager.newTask(0, "Low Priority task overdue", "This is a sample task", new sub(Date.now(), {"days": 1}), "low");
+    taskManager.newTask(0, "Regular task without date", "This is a sample task", null, null);
 
-taskManager.newTask(0, "High Priority task today", "This is a sample task",  new Date(Date.now()), "high");
-// task with date
-taskManager.newTask(0, "Medium Priority task upcoming", "This is a sample task", new add(Date.now(), {"years": 1}), "medium");
-// tasks with different priorities
-taskManager.newTask(0, "Low Priority task overdue", "This is a sample task", new sub(Date.now(), {"days": 1}), "low");
-taskManager.newTask(0, "Regular task without date", "This is a sample task", null, null);
+    // save tasks
+    saveTasks(taskManager);
+}
 
 let domController = new DomController(taskManager);
 
