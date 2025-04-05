a99db6a86d02724a3527beee9fa72f29114bbce2
john-kelly <johnkellytemena@gmail.com>
2025-04-05 20:56:17 +0800

    Add load and save functions

---
+++ src/modules/loadTasks.js
@@ -0,0 +1,49 @@
+import Project from "./project";
+import Task from "./task";
+import TaskManager from "./task-manager";
+
+export default function () {
+    let saveFile = localStorage.getItem("saveFile");
+    
+    if (saveFile) {
+        let taskManager = new TaskManager();
+        taskManager.taskIdCounter = saveFile.taskIdCounter;
+        taskManager.projectIdCounter = saveFile.projectIdCounter;
+        
+        let newProjectList = {};
+
+        saveFile = JSON.parse(saveFile);
+
+        let projectList = saveFile.projectList;
+
+        Object.entries(projectList).forEach(([projectId, project]) => {
+
+            // create new project class
+            let newProject = new Project(project.title);
+
+            Object.entries(project.taskList).forEach(([taskId, task]) => {
+                task.dueDate = task.dueDate ? new Date(task.dueDate) : null;
+
+                // create new task class from object
+                let newTask = new Task(task.title);
+                newTask.description = task.description;
+                newTask.dueDate = task.dueDate;
+                newTask.priority = task.priority;
+                newTask.isComplete = task.isComplete
+
+                // add task to project
+                newProject.addTask(newTask, taskId);
+            });
+
+            // add project to project list
+
+            newProjectList[projectId] = newProject;
+        });
+
+        taskManager.projectList = newProjectList;
+
+        return taskManager;
+    } else {
+        return;
+    }
+}
---
+++ src/modules/saveTasks.js
@@ -0,0 +1,11 @@
+export default function (taskManager) {
+    // create savefile
+
+    let saveFile = {
+        "taskIdCounter": taskManager.taskIdCounter,
+        "projectIdCounter": taskManager.projectIdCounter,
+        "projectList": taskManager.getProjects()
+    }
+
+    localStorage.setItem("saveFile", JSON.stringify(saveFile));
+}
